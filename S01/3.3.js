0176a82961d2811663272b94e1d91e6a7797da39
Andreea Dioclitean <diocliteanandreea21@stud.ase.ro>
2023-12-14 01:16:44 +0200

    S01 - Introduction to JavaScript

---
+++ S01/3.3.js
@@ -0,0 +1,5 @@
+let occurences = (text, character) =>
+    text.split(character).length - 1
+
+
+console.log(occurences("sample text", "e"));
---
+++ S01/4.2.js
@@ -0,0 +1,11 @@
+
+function addToArray(array, ...args) {
+    for (var i = 0; i < args.length; i++) {
+        array.push(args[i]);
+    }
+
+    return array
+}
+
+let array = ["a"]
+console.log(addToArray(array, "b", "c").join(", "))
